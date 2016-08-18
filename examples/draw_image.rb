06db91411b6d50728f3d2371d570fbd48e37b806
Elias Fröhner <elias.froehner@gmx.de>
2016-08-18 09:45:01 +0200

    Fixed examples

--- examples/draw_image.rb
+++ examples/draw_image.rb
@@ -1,7 +1,7 @@
 require 'better_show'
 
 @ctx = BetterShow::ScreenContext.new
-
+@ctx.erase_rows(0, @ctx.get_rows)
 @ctx.set_rotation(BetterShow::Screen::ROTATION_WEST)
 @ctx.draw_image("../test/files/penguin.raw")
 @ctx.flush!
--- examples/hello_world.rb
+++ examples/hello_world.rb
@@ -2,7 +2,8 @@ require 'better_show'
 
 @ctx = BetterShow::ScreenContext.new
 
-@ctx.reset_screen
+@ctx.erase_screen
+@ctx.restore_defaults
 @ctx.set_rotation(BetterShow::Screen::ROTATION_NORTH)
 @ctx.set_background_color(:white)
 @ctx.set_foreground_color(:red)
